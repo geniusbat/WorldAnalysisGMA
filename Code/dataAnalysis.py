b12ca1d3e563e907145983d995e801be8dbb533b
unknown <kahmuthefreeone@gmail.com>
2022-06-17 11:32:51 +0200

    nor

--- Code/dataAnalysis.py
+++ Code/dataAnalysis.py
@@ -1,7 +1,10 @@
 from cProfile import label
+from os import scandir
 import matplotlib.pylab as plt
 import numpy as np
 import pandas as pd
+from sklearn import preprocessing
+
 processedDataDir = "ProcessedData"
 availableYears = ["2015", "2016", "2017", "2018", "2019"]
 
@@ -10,8 +13,15 @@ spainData = [dfGdp.loc[dfGdp["Country Name"]=="Spain"][year] for year in availab
 germanData = [dfGdp.loc[dfGdp["Country Name"]=="Germany"][year] for year in availableYears]
 x = availableYears
 
+scaler = preprocessing.RobustScaler()
+spainScaled = scaler.fit_transform(np.array(spainData).reshape(-1,1))
+germanScaled = scaler.fit_transform(np.array(germanData).reshape(-1,1))
+
+
+
 plt.figure()
-plt.plot(x, spainData, label="Spain")
-plt.plot(x, germanData, label="Germany")
+#plt.plot(x, spainData, label="Spain")
+plt.plot(x, spainScaled, label="Spain scaled")
+plt.plot(x, germanScaled, label="Germany")
 plt.legend()
 plt.show()
